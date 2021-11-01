a94498004ece7f1a7571beba71355a2b4f73e7a1
afreimanis1991 <afreimanis1991@inbox.lv>
2021-11-01 20:55:10 +0300

    01.11.2021

---
+++ Class_15/JS/main.js
@@ -0,0 +1,26 @@
+//myGreeting();
+
+//myGreeting();
+
+function myGreeting() {
+    alert('hello');
+}
+
+const myGreeting_announymes = function(){
+alert('hello');
+}
+
+const myButton = document.querySelectorAll('button');
+
+// myButton.forEach(onclick = function (value, index, array) {
+//     console.log(array);
+//     alert('hello from selector (all buttons - ' +index+' - element)');
+
+// });
+
+myButton.forEach((element, index, array) => onclick = function() {
+        alert('hello from selector (all buttons)');
+        console.log(array);
+});
+
+//myGreeting();
