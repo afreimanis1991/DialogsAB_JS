7d56dc227dd7b52b78fec675ec5d50dfaa245e4b
fraidman1991 <afreimanis1991@inbox.lv>
2022-01-26 21:24:00 +0200

    updates

--- 26.01.2022/JS/isInteger.test.js
+++ 26.01.2022/JS/isInteger.test.js
@@ -22,3 +22,21 @@ test("isInteger fails for non-integer symbol value", () => {
 });
 
 
+const integerNumbers = [-1, 0, 1];
+test.each(integerNumbers)(
+    "isInteger passes for integer value %j from array (only true  is waited for)",
+    (fixture) => expect(isInteger(fixture)).toBe(true)
+);
+
+const Numbers = [
+  [-1, true],
+  [0, true],
+  [1.23, false]
+];
+
+test.each(Numbers)(
+    "isInteger passes for numeric value %j from array with result %j",
+    (fixture, result) => expect(isInteger(fixture)).toBe(result)
+);
+
+
